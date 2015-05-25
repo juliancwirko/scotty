a9f41c6eeb350327432c4d91872d685bf8f4ea96
Julian Ćwirko <julian.cwirko@gmail.com>
2015-05-25 14:16:49 +0200

    updates

--- client/subscriptions/subscriptions.js
+++ client/subscriptions/subscriptions.js
@@ -1,3 +1,3 @@
 'use strict';
 
-// your subscriptions here or you can place it in FlowRouter too
+// your subscriptions here or you can place it in FlowRouter too (it depends what you want to achieve)
