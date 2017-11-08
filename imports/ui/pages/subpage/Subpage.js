9d95d4d0058a5bde160faa5c3bd0c98498fd7bce
Brian <bdombro@gmail.com>
2017-11-08 11:20:50 -0500

    Fixed dependencies and also fixed Subpage to work and match home syntax. It didnt work before.

--- imports/ui/pages/subpage/Subpage.js
+++ imports/ui/pages/subpage/Subpage.js
@@ -1,27 +1,58 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
+import { array, func } from 'prop-types';
 
-class Subpage extends React.Component {
-  state = {
-    testData: ['dasdsa', 'fdsfdsfds', 'dadasdsa'],
-  }
-  render() {
-    return (
-      <div className="subpage">
-        <Helmet>
-          <title>Subpage</title>
-          <meta name="description" content="This is subpage. Just Helmet SSR demo for other page" />
-          <meta property="og:title" content="This is subpage. Just Helmet SSR demo for OG" />
-        </Helmet>
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        </div>
-        <div>
-          {this.state.testData.map((i, index) => <div key={index}>{i}</div>)}
-        </div>
+
+// class Subpage extends React.Component {
+//   state = {
+//     testData: ['dasdsa', 'fdsfdsfds', 'dadasdsa'],
+//   }
+//   render() {
+//     return (
+//       <div className="subpage">
+//         <Helmet>
+//           <title>Subpage</title>
+//           <meta name="description" content="This is subpage. Just Helmet SSR demo for other page" />
+//           <meta property="og:title" content="This is subpage. Just Helmet SSR demo for OG" />
+//         </Helmet>
+//         <div>
+//           Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+//         </div>
+//         <div>
+//           {this.state.testData.map((i, index) => <div key={index}>{i}</div>)}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+const Subpage = (props) => {
+  const { testData } = props;
+  return (
+    <div className="subpage">
+      <Helmet>
+        <title>Subpage</title>
+        <meta name="description" content="This is subpage. Just Helmet SSR demo for other page" />
+        <meta property="og:title" content="This is subpage. Just Helmet SSR demo for OG" />
+      </Helmet>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
       </div>
-    );
-  }
+      <div>
+        {testData.map((i, index) => <div key={index}>{i}</div>)}
+      </div>
+    </div>
+  );
 }
 
-export default Subpage;
+Subpage.propTypes = {
+  testData: ['dasdsa', 'fdsfdsfds', 'dadasdsa'],
+};
+
+const mapStateToProps = state => ({ testData: ['dasdsa', 'fdsfdsfds', 'dadasdsa'] });
+const mapDispatchToProps = dispatch => ({
+  // dispatchCallAddTodo: data => dispatch(callAddTodo(data)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Subpage);
