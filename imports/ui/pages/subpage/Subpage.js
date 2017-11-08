3a3afd408eabccbf881cdd0b7a14eb8563d3630b
Brian <bdombro@gmail.com>
2017-11-08 11:23:33 -0500

    Removed commented out code

--- imports/ui/pages/subpage/Subpage.js
+++ imports/ui/pages/subpage/Subpage.js
@@ -3,30 +3,6 @@ import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
 import { array, func } from 'prop-types';
 
-
-// class Subpage extends React.Component {
-//   state = {
-//     testData: ['dasdsa', 'fdsfdsfds', 'dadasdsa'],
-//   }
-//   render() {
-//     return (
-//       <div className="subpage">
-//         <Helmet>
-//           <title>Subpage</title>
-//           <meta name="description" content="This is subpage. Just Helmet SSR demo for other page" />
-//           <meta property="og:title" content="This is subpage. Just Helmet SSR demo for OG" />
-//         </Helmet>
-//         <div>
-//           Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-//         </div>
-//         <div>
-//           {this.state.testData.map((i, index) => <div key={index}>{i}</div>)}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
 const Subpage = (props) => {
   const { testData } = props;
   return (
